9d7f0f8412d69a5aa07de65eec32fac653759dfa
clarkem8 <34151314+clarkem8@users.noreply.github.com>
2020-11-28 23:47:26 +0000

    Update ssh_keygen.sh

--- git_setup/ssh_keygen.sh
+++ git_setup/ssh_keygen.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# $1 - Name
+# $2 - email
+
 ssh-keygen -t rsa -b 4096 -f ~/.ssh/github.key
 echo "Host github.com" >> ~/.ssh/config
 echo "  IdentityFile ~/.ssh/github.key" >> ~/.ssh/config
 
 chmod -R 600 ~/.ssh/*
+
+git config --global user.name $1
+git config --global user.email $2
 
 # The public ssh key needs to be copied into your github project
