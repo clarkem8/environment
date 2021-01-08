a41bd344e7eac365d0a5fcfcbc4d5d69c7b20837
clarkem8 <34151314+clarkem8@users.noreply.github.com>
2021-01-08 21:59:40 +0000

    Update user_setup.sh

--- setup/user_setup.sh
+++ setup/user_setup.sh
@@ -34,8 +34,3 @@ fi
 # Install miniconda and initialise
 $base_path/miniconda_install.sh
 /home/$USER/miniconda/bin/conda init
-
-if [[ $INSTALL_TEST_CONDA_ENV == "true" ]]; then
-    # Install test conda environment
-    $GIT_PATH/conda_envs/create_env.sh $GIT_PATH/conda_envs/yaml_files/test.yml
-fi
