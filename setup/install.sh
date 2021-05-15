62b33db4dd9b90b851a2f245793444f717e9a154
clarkem8 <34151314+clarkem8@users.noreply.github.com>
2021-05-15 16:36:36 +0100

    Update install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -6,17 +6,18 @@ base_path=$(dirname $(readlink -f $0))
 # Add user
 useradd $USER
 
+GIT_REPO=environment
+
 if [[ "$CLI_INSTALL" == "Yes" ]];then
     # Install git and clone repo
     yum install git -y
-    GIT_REPO=environment
-    export GIT_PATH=/home/$USER/github/
+    export GIT_PATH=/home/$USER/github
     echo "Git path is - $GIT_PATH"
     sudo -i -u $USER git clone "https://github.com/clarkem8/$GIT_REPO.git" $GIT_PATH/$GIT_REPO
 
     # Install packages and run user install
-    $GIT_PATH/setup/install_packages.sh
-    sudo -i -u $USER $GIT_PATH/setup/user_setup.sh
+    $GIT_PATH/$GIT_REPO/setup/install_packages.sh
+    sudo -i -u $USER $GIT_PATH/$GIT_REPO/setup/user_setup.sh
 else
     # Install packages
     $base_path/install_packages.sh
